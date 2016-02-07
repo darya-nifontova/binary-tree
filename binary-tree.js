5420c8be504ee18433c25de83845295fb7e005d0
someone <someone@someplace.com>
2016-02-07 12:58:13 +0300

    Add constructor, insert

--- binary-tree.js
+++ binary-tree.js
@@ -2,12 +2,33 @@
 
 class BinaryTree {
 	constructor() {
-
+		this.root = null;
 	}
 
 	insert(data) {
+        if (this.root == null) 
+            {this.root = new Node(data)
+            }
+        else {
+            var current = this.root;
+            var F = false;
 
-	}
+            while (!F) {
+                if (data < current.data) {
+                    if (current.left == null) {      
+                        current.left = new Node(data);
+                        F = true;
+                    } else current = current.left;
+                } 
+                else {
+                    if (current.right == null) {
+                        current.right = new Node(data);
+                        F = true;
+                    } else current = current.right;   
+                }
+            }
+        }
+    }
 
 	contains(data) {
 
